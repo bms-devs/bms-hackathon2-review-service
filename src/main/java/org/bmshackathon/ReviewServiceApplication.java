490a470f3c91a3e817c407d22f3a942f24d46b9c
p-bms <p-bms@users.noreply.github.com>
2015-09-26 13:58:59 +0200

    Added Spring Cloud Sleuth

--- src/main/java/org/bmshackathon/ReviewServiceApplication.java
+++ src/main/java/org/bmshackathon/ReviewServiceApplication.java
@@ -3,10 +3,18 @@ package org.bmshackathon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ReviewServiceApplication {
+
+    @Bean
+    public Sampler<?> defaultSampler() {
+        return new AlwaysSampler();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReviewServiceApplication.class, args);
