6e5b21a8a016c6b3ca87d74cd8cf897934dc800c
lelenp <lelenp@ee.pw.edu.pl>
2015-09-26 11:58:05 +0200

    fix naming strategy in init data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into Review(id, text, clientFullName, rating, videoImageId)
+insert into Review(id, text, client_full_name, rating, video_image_id)
 VALUES (
   1,
   'First image review',
@@ -6,7 +6,7 @@ VALUES (
   10,
   1);
 
-insert into Review(id, text, clientFullName, rating, videoImageId)
+insert into Review(id, text, client_full_name, rating, video_image_id)
 VALUES (
   2,
   'Second image review',
