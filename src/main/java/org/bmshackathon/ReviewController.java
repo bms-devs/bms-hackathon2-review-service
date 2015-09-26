52bf98a4cab4541f6181252a0e1005f58196cf23
Mateusz Gałan <galan.mateusz@gmail.com>
2015-09-26 16:56:08 +0200

    404 fix for empty list

--- src/main/java/org/bmshackathon/ReviewController.java
+++ src/main/java/org/bmshackathon/ReviewController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -27,7 +28,7 @@ public class ReviewController {
     @RequestMapping(value = "/findByVideoId/{videoId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Review>> findByVideoId(@PathVariable Long videoId) {
         List<Review> reviewList = reviewRepository.findByVideoImageId(videoId);
-        return reviewList.isEmpty() ? new ResponseEntity<>(HttpStatus.NOT_FOUND) :
+        return reviewList.isEmpty() ? new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK) :
                 new ResponseEntity<>(reviewList, HttpStatus.OK);
     }
 }
